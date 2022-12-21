0693e64a2f6e7a4dae602926be1bb966dabccaf2
Saquibshadab001 <120798023+Saquib0001@users.noreply.github.com>
2022-12-21 22:32:07 +0530

    Add files via upload

---
+++ helloworld&sum2no.c
@@ -0,0 +1,45 @@
+// method 1
+#include<stdio.h>
+void saquib(){
+    printf("hello world!!!!");
+}
+
+int main(){
+    saquib();
+    return 0;
+}
+
+//method 1 
+
+#include<stdio.h>
+void sum(){
+int s;
+s = 4+5;
+printf("the sum of two no is : %d" ,s);
+}
+
+int main(){
+    sum();
+    return 0;
+}
+
+// method 2
+#include<stdio.h>
+int main(){
+    printf("hello world welcome to world of programming");
+    return 0;
+}
+
+
+//method 2
+#include<stdio.h>
+int main(){
+    int a,b,s;
+    a = 5 ;
+    b = 6 ;
+    s = a+b;
+    printf("the sum of a and b : %d",s);
+    return 0;
+}
+
+
