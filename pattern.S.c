a2d29c31a5b340f9c51ba29a0a8fe249e629f869
Saquibshadab001 <120798023+Saquib0001@users.noreply.github.com>
2022-12-21 23:11:07 +0530

    Add files via upload

---
+++ pattern.S.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+int main(){
+
+
+    printf("********\n");
+    printf("*\n");
+     printf("*\n");
+      printf("*\n");
+       printf("*\n");
+       
+    printf("********\n");
+     printf("       *\n");
+     printf("       *\n");
+     printf("       *\n");
+     printf("       *\n");
+    
+    printf("********\n");
+    return 0;
+
+
+}
