39f8dbe3dcecfd85c3f65bb30bee62ffd951e837
Saquibshadab001 <120798023+Saquib0001@users.noreply.github.com>
2022-12-26 19:16:09 +0530

    Add files via upload

---
+++ insertarray.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    
+        int size;
+        printf("enter the size of an array : \n");
+        scanf("%d",&size);
+        int arr[size] ;
+    printf("insert an element in array :\n");
+    for(int i=0 ; i<size ; i++){
+
+        scanf("%d\n",&arr[i]);
+    }
+        printf("printing the value in present in array : \n");
+    for(int i = 0; i<size; ++i){
+
+        printf("%d\n",arr[i]);
+    }
+    return 0;
+}
