0e02fdf71ea4ff715243f1c3721bb72b9896ef96
Saquibshadab001 <120798023+Saquib0001@users.noreply.github.com>
2022-12-29 21:46:15 +0530

    Add files via upload

---
+++ string_length.c
@@ -0,0 +1,14 @@
+// program to count string length
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+   
+   char name[] = "Saquib";
+   int length = strlen(name);
+
+   printf("string length are : %d " ,length);
+
+   return 0 ;
+
+}
