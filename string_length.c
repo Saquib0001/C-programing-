3f5e44cd08fa1472c7186eb1adfca6176b54d915
Saquibshadab001 <120798023+Saquib0001@users.noreply.github.com>
2022-12-29 22:24:13 +0530

    Add files via upload

--- string_length.c
+++ string_length.c
@@ -1,4 +1,27 @@
-// program to count string length
+//method 1
+// take input from user
+#include<stdio.h>
+int countlength(char arr[]); // function prototype
+  
+    int main(){
+       char name[100];
+       fgets(name , 100 , stdin); // fgets take input from user
+       printf(" string length are : %d " ,countlength(name));
+       return 0;
+    }
+
+
+  int countlength(char arr[]){
+       int count = 0;
+       for(int i=0 ; arr[i] != '\0' ; i++){
+          count++ ;
+       }
+       return count - 1 ; //due to null char '\0'       
+    }
+
+
+//method 2
+// program to count string length using string header file..
 #include<stdio.h>
 #include<string.h>
 
